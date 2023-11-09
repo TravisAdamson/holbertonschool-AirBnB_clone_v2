659faab800d44ae5de69cfa9d0db43396268054e
Travis Adamson <tndadamson@gmail.com>
2023-11-09 07:28:29 -0600

    Adding additional route for optional prompt

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Initializes an initial flask web app
+
+Listen: 0.0.0.0, port 5000
+Route:
+    /: Display 'Hello HBNB!'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Display 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_bnb():
+    """ Displays only 'HBNB' """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
