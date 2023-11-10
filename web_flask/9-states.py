96247aca6f6b65d822767d49116be401f7f46820
Travis Adamson <tndadamson@gmail.com>
2023-11-10 08:55:35 -0600

    Fixed indent

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -27,7 +27,7 @@ def states_id(id):
     for state in storage.all('State').values():
         if state.id == id:
             return render_template('9-states.html', state=state)
-        return render_template('9-states.html')
+    return render_template('9-states.html')
 
 
 @app.teardown_appcontext
